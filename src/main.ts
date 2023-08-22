374f15e50a966fc225a81a6565e7b19f28035eef
areverdy <alec.rev1234@gmail.com>
2023-08-22 16:15:58 +0200

    ajout 2e sous-titre

--- src/main.ts
+++ src/main.ts
@@ -35,12 +35,12 @@ app.appendChild(partie_haute)
 
 const partie_basse = document.createElement('div')
 partie_basse.classList.add("partie_basse")
+partie_basse.setAttribute('id', '2eboite')
 
-const list = document.createElement('div')
-list.innerText = 'liste_des_taches'
-list.classList.add('class')
-partie_basse.appendChild(list)
-app.appendChild(partie_basse)
+// const list = document.createElement('div')
+// list.innerText = 'liste_des_taches'
+// partie_basse.appendChild(list)
+// app.appendChild(partie_basse)
 
 button.addEventListener("click",()=> {
   afficher_nouvelle_tache()
